37209bbd42d47080d69709ab625c7c1c3af41d37
konkon <konkon@gmail.com>
2024-04-04 15:50:41 +0530

    second commit

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,13 @@
+import './login.css'
+const Login = () => {
+    return(
+        <>
+        <div className='textfield-container'>
+            <label>Username</label>
+            <input type="text" />
+        </div>
+        
+        </>
+    )
+}
+export default Login;
